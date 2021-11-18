89588543695b385f7f0de97ce9c67b0641ef065a
mateiocristi <mateiocristi@gmail.com>
2021-11-18 15:18:43 +0200

    [matei] work on category selection

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -38,36 +38,36 @@ public class Initializer implements ServletContextListener {
         }
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        Supplier asus = new Supplier("Asus", "Laptops");
-        try {
-            supplierDataStore.add(amazon);
-            supplierDataStore.add(lenovo);
-            supplierDataStore.add(asus);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //setting up a new product category
-        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "Laptop. A PC packed into a smaller, more practical shape");
+//        Supplier amazon = new Supplier("Amazon", "Digital content and services");
+//        Supplier lenovo = new Supplier("Lenovo", "Computers");
+//        Supplier asus = new Supplier("Asus", "Laptops");
+//        try {
+//            supplierDataStore.add(amazon);
+//            supplierDataStore.add(lenovo);
+//            supplierDataStore.add(asus);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        //setting up a new product category
+//        ProductCategory laptop = new ProductCategory("Laptop", "Hardware", "Laptop. A PC packed into a smaller, more practical shape");
 //        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        try {
+//        try {
 //            productCategoryDataStore.add(tablet);
-              productCategoryDataStore.add(laptop);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            productDataStore.add(new Product("Asus TUF FX505DU Gaming Laptop", new BigDecimal("399.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", laptop, asus));
+//            productCategoryDataStore.add(laptop);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            productDataStore.add(new Product("Asus TUF FX505DU Gaming Laptop", new BigDecimal("399.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", laptop, asus));
 //            productDataStore.add(new Product("Lenovo Pad", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, lenovo));
 //            productDataStore.add(new Product("Lenovo IdeaPad Mix 700", new BigDecimal("479"), "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
 //            productDataStore.add(new Product("Amazon Fire HD 8", new BigDecimal("89"), "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
 //            productDataStore.add(new Product("Asus ZenPad", new BigDecimal("49.9"), "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, asus));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
 
 //
 
